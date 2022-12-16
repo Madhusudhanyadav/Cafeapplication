d74cc04021604a3da21e60c428e30f3e10020cb6
Madhusudhanyadav <105106182+Madhusudhanyadav@users.noreply.github.com>
2022-12-16 20:35:29 +0530

    "c1"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
-import Navbar from "./components/homepage/Navbar";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+// import { Route } from "react-router-dom";
 
 function App() {
-  const head = (
-    <img
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGHOGGm58kde1ka7aqEettaP_i0VuS82on5gRD1mHaFw&s"
-      alt="cafeApplication" width={20}
-    ></img>
-  );
+  
   return (
-    <>
-      <Navbar title={head} />
-    </>
+    <div>
+  
+      <Navbar title="Cafe Application"/>
+      <Homepage />
+    </div>
   );
 }
 
