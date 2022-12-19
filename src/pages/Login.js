914d44795cb665f8b4f7da228a4457a9e9eee44d
Madhusudhanyadav <105106182+Madhusudhanyadav@users.noreply.github.com>
2022-12-19 22:16:12 +0530

    "dec-19"

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,17 @@
 // TODO
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
   return (
-    <div>
-      
-    </div>
-  )
+    
+      <div>
+        
+      </div>
+    
+
+
+
+
+
+  );
 }
--- src/userpages/Place.js
+++ src/userpages/Place.js
@@ -2,21 +2,52 @@ import React from "react";
 import "../styles/Place.css";
 import { useState,useEffect } from 'react';
 import axios from "axios";
+import Toastify from 'toastify-js';
+import "toastify-js/src/toastify.css";
+
+
+
 
 export default function Place() {
 
+
+
   // States
   const [items,setItems] = useState([]);
+  // no of items for each item id
   const [itemsColl,setItemsColl] = useState({});
   const [tb_no,setTb_no] = useState(0);
   const [c_name,setC_name] = useState("");
   const [phno,setPhno] = useState("");
+  const [ord_no,setOrd_no]=useState(0);
 
+  // Toast messages
+  const toast=(text)=>{
+    Toastify({
+      text: text,
+      duration: 3000,
+      
+      offset: {
+        // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: 50 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+    }).showToast();
+   }
 
   // Handle on change for phon
   const handleOnChange = (event)=>{
-    console.log(event.target.parentElement.parentElement);
-    if(event.target.name === "tb_no"){
+    // console.log(event.target.parentElement.parentElement);
+    if(event.target.name==="ord_no"){
+      
+      setOrd_no(event.target.value);
+    }
+    else if(event.target.name === "tb_no"){
       
       setTb_no(event.target.value);
     }
@@ -38,19 +69,71 @@ export default function Place() {
   const handleOnClick=(event)=>{
    if(event.target.checked){
     event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.removeAttribute('disabled',false);
+    event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.value="1";
+    let tpColl=itemsColl;
+    tpColl[event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.id]=1;
+    setItemsColl(tpColl);
+    console.log(itemsColl);
    }
    else{
     event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.value="";
+    let tpColl=itemsColl;
+    tpColl[event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.id]=0;
+    setItemsColl(tpColl);
+    console.log(itemsColl);
     event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.setAttribute('disabled',false);
    }
-  //  console.log(event.target.parentElement.nextElementSibling.nextElementSibling.firstElementChild.className);
-   
-
   }
 
+ // form submission
+  const handleOnSubmit = (event)=>{
+    event.preventDefault();
+    if(event.target.className === "myform2"){
+      const url="http://localhost:8080/user/get/status?ord_no="+ord_no;
+      
+      axios.get(url).then((response)=>{
+        toast(response.data);
+      })
+      console.log(event.target.className);
+    }
+    else{
+      let currOrder=[];
+      for (const key in itemsColl) {
+        if(itemsColl[key]!==0){
+          currOrder.push({"id":`${key}`,"quantity":`${itemsColl[key]}`});
+        }
+        
+      }
+      console.log(currOrder);
+      // alert("submitted");
+      let obj={
+        "tb_no":tb_no,
+        "c_name":c_name,
+        "phno":phno,
+        "items":currOrder
+      }
+      // toast("hello");
+      
+      axios.post("http://localhost:8080/user/post/order",obj).then((response)=>{
+        toast("Your order no is "+response.data);
+        // navigate("/login");
+
+        // console.log(response);
+      }).catch((err)=>{
+        console.log(err);
+      })
+      event.target.reset();
+      let elements=document.getElementsByClassName("checkBox");
+      for(let ele of elements){
+        ele.setAttribute("disabled",true);
+        // console.log(ele);
+      }
+      // console.log(elements);
+    }
+  }
   useEffect(()=>{
     axios.get("http://localhost:8080/user/get/items").then((response)=>{
-      const tp=itemsColl;
+      let tp=itemsColl;
       console.log(response.data);
       setItems(response.data);
       response.data.map((element)=>{
@@ -63,18 +146,19 @@ export default function Place() {
         // tp.push(obj);
         setItemsColl(tp);
         console.log(itemsColl);
+        return 0;
       })
       
      
     })
     
-  },[])
+  },[itemsColl])
 
   
 
   return (
     <div className="container my-3">
-      <form className="myform">
+      <form className="myform" method="post" onSubmit={handleOnSubmit}>
         <center><h1>Place your order</h1></center>
         <div className="input-group mb-3 px-3">
           <span className="input-group-text" id="inputGroup-sizing-default">
@@ -133,7 +217,7 @@ export default function Place() {
            
             
                   <div>Price : {element.price}</div>
-                  <div>Quantity : <input type={"number"} style={{width:"30%"}} id={element.id} className="" disabled onChange={handleOnChange}/></div>
+                  <div>Quantity : <input type={"number"} style={{width:"30%"}} id={element.id} className="checkBox" disabled onChange={handleOnChange}/></div>
 
                   <div>
                     <img src={element.image} alt="" style={{width:"5rem",height:"5rem",borderRadius:"5px"}}/>
@@ -158,9 +242,35 @@ export default function Place() {
             <div>
               <img src="https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg" alt="" style={{width:"5rem",height:"5rem",borderRadius:"5px"}}/>
             </div> */}
-
+          
         </div>
-        
+        <div className="col-12">
+          <center>
+          <button className="btn btn-primary m-3" type="submit">Place Order</button>
+          </center>
+        </div>
+      </form>
+      <form className="myform2" onSubmit={handleOnSubmit}>
+        <center><h2>Check Status</h2></center>
+      <div className="input-group mb-3 px-3">
+          <span className="input-group-text" id="inputGroup-sizing-default">
+            Your Order no
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default"
+            name="ord_no"
+            onChange={handleOnChange}
+            required
+          />
+        </div>
+        <div className="col-12">
+          <center>
+          <button className="btn btn-primary m-3" type="submit">Get Status</button>
+          </center>
+        </div>
       </form>
     </div>
   );
